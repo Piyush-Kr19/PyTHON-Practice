e76e282399875fde24d152f947c7118e717bac52
Piyush-Kr-19 <piyush14359@gmail.com>
2026-08-25 10:51:37 +0530

    feat: add ATM simulator with deposit and withdrawal validation

--- Basics/Practice/6.py
+++ Basics/Practice/6.py
@@ -9,3 +9,25 @@ balance = int(input("Enter the balance: "))
 
 print(f"Balance: ${balance}")
 
+print("1. Check Balance")
+print("2. Deposit")
+print("3. Withdraw")
+choice = int(input("Choose an option: "))
+
+if choice == 1:
+    print(f"Balance: ${balance}")
+    
+elif choice == 2:
+    depoist = int(input("Enter the amount to be depoisted: "))
+    balance += depoist
+    print(f"Balance: ${balance}")
+    
+elif choice == 3:
+    withdraw = int(input("Enter the amount to withdraw: "))
+    if withdraw > balance:
+            print("Insufficient Balance")
+    else:
+        balance -= withdraw
+        print(f"Balance: ${balance}")
+else:
+    print("Invalid option")
